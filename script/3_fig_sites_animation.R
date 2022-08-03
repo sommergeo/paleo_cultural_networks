e150925c5a58f8e49d69c80600fdfacab2c799d0
sommergeo <26389306+sommergeo@users.noreply.github.com>
2022-08-03 14:20:12 +0200

    create presentation figs

--- script/3_fig_sites_animation.R
+++ script/3_fig_sites_animation.R
@@ -1,13 +1,13 @@
 library(tidyverse)
 library(gganimate)
-library(ggfx)
 
 d <- readRDS('data/ROAD_table.rds') %>% rename(query_age_min=age_min, query_age_max=age_max, locality.idlocality=locality, locality.x=x, locality.y=y) %>% 
   drop_na(c(query_age_min,query_age_max, locality.x, locality.y)) %>% 
-  mutate(steps=as.integer(cut(query_age_min, # cut in 10ka-slices
-                              breaks= seq(-1000,3000000,10000), label=FALSE)*10)) %>% 
-  #mutate(steps_exp=log(steps))
-  mutate(steps_exp=log(query_age_max), steps_dur=log(query_age_max)-log(query_age_min))
+  filter(query_age_min<3000000) %>% 
+  mutate(query_age_max = replace(query_age_max, query_age_max>3000000, 3000000)) %>% 
+  mutate(steps_exp=log(query_age_max), 
+         steps_dur=log(query_age_max)-log(query_age_min),
+         age_dur=query_age_max-query_age_min)
 
 ## World map from natural earth (rendering 10x faster)
 world_shape <- rnaturalearth::ne_countries(scale = 110, type = 'countries', continent = c('europe', 'africa', 'asia', 'oceania'), returnclass = 'sp')
@@ -24,7 +24,8 @@ plot(world)
 map <- world +
   geom_point(data=d, aes(x=locality.x, y=locality.y, group=locality.idlocality),
              size=2, color='#4472c4', alpha = 1, show.legend=F)+
-  transition_time(time=-d$steps_exp, ) +
+  #transition_time(time=-d$steps_exp, range=d$steps_dur) +
+  transition_time(time=-d$query_age_max, range=d$age_dur) +
   shadow_mark(past=TRUE, size=2, colour="grey30", alpha=.1)+
   enter_fade() +
   exit_fade() +
